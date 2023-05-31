559137f854e8a8a79b3a4751bc2d3fdce8d5cbb4
ACarfi <carfi.alessandro@gmail.com>
2023-05-31 17:48:50 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,6 @@
-
+void init (char mat[], int n);
+void init(char mat[5][15]);
+void stampa(char n[5][15], char c[5][15]);
+void inserimento(char mat[], int n);
+void inserimento(char nomi[5][15], char cognomi[5][15]);
+int ricerca(char nomi[5][15], char cognomi[5][15], char nome[15], char cognome[15]);
