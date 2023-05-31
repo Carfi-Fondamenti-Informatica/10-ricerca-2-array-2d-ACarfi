8a89f70cf57f20ebda3b58cf76b3cc6897eb2ced
ACarfi <carfi.alessandro@gmail.com>
2023-05-31 17:39:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,80 @@
 #include <iostream>
+using namespace std;
 
-int main(){
-  return 0;
+void init (char mat[], int n){
+    for(int i=0;i<n;i++){
+        mat[i]= ' ';
+    }
+}
+void init(char mat[5][15]){
+    for(int i=0;i<5;i++){
+        init(mat[i],15);
+    }
+}
+
+void stampa(char n[5][15], char c[5][15]){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<15;j++){
+            cout << n[i][j];
+        }
+        cout << " ";
+        for(int j=0;j<15;j++){
+            cout << c[i][j];
+        }
+        cout <<endl;
+    }
+}
+
+void inserimento(char mat[], int n){
+    char temp_m[n*10];
+    init(temp_m,n*10);
+    cin >> temp_m;
+    for(int i=0;i<n;i++){
+        mat[i] = temp_m[i];
+    }
+}
+
+void inserimento(char nomi[5][15], char cognomi[5][15]){
+    for(int i=0; i<5;i++){
+        inserimento(nomi[i],15);
+        inserimento(cognomi[i],15);
+    }
+}
+
+int ricerca(char nomi[5][15], char cognomi[5][15], char nome[15], char cognome[15]){
+    for(int i=0;i<5;i++){
+        int j=0;
+        for(j=0;j<15;j++){
+            if(nomi[i][j]!=nome[j]){
+                break;
+            }
+            if(cognomi[i][j]!=cognome[j]){
+                break;
+            }
+        }
+        if(j==15){
+            return i;
+        }
+    }
+    return -1;
+}
+int main() {
+    char nomi[5][15], cognomi[5][15];
+    init(nomi);
+    init(cognomi);
+    inserimento(nomi, cognomi);
+
+    char nome[15], cognome[15];
+
+    inserimento(nome, 15);
+    inserimento(cognome, 15);
+
+    int id = ricerca(nomi, cognomi, nome, cognome);
+
+    if (id != 1) {
+        cout << "trovato in posizione " << id << endl;
+    }else{
+        cout << "non trovato" <<endl;
+    }
+    return 0;
 }
